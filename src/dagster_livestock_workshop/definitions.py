3e0fb3134c9b92c5b84e1e4e1d616f592602c5ba
amantaya <aantaya@email.arizona.edu>
2026-08-08 09:21:13 -0600

    feat: include weather API demo in asset definitions

--- src/dagster_livestock_workshop/definitions.py
+++ src/dagster_livestock_workshop/definitions.py
@@ -1,10 +1,20 @@
 from dagster import Definitions
 
-from .assets import livestock_reference_api, r_postprocess_demo, scheduled_livestock_api
+from .assets import (
+    livestock_reference_api,
+    r_postprocess_demo,
+    scheduled_livestock_api,
+    weather_api_demo,
+)
 from .schedules import daily_livestock_schedule, scheduled_livestock_job
 
 defs = Definitions(
-    assets=[livestock_reference_api, scheduled_livestock_api, r_postprocess_demo],
+    assets=[
+        livestock_reference_api,
+        scheduled_livestock_api,
+        r_postprocess_demo,
+        weather_api_demo,
+    ],
     jobs=[scheduled_livestock_job],
     schedules=[daily_livestock_schedule],
 )
